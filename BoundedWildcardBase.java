c75ba3bdefb0517d576e49c3542f0cb49d9bc4b8
injoon2019 <injoon2019@gmail.com>
2019-02-01 00:03:57 -0800

    Feb 1st, 2019

---
+++ BoundedWildcardBase.java
@@ -0,0 +1,30 @@
+class Box<T>{
+	private T ob;
+	public void set(T o){ob=o;}
+	public T get(){return ob;}
+}
+
+class Toy{
+	@Override
+	public String toString(){
+		return "I am a Toy";
+	}
+}
+
+class BoxHandler{
+	public static void outBox(Box<Toy> box){
+		Toy t = box.get();	//t
+		System.out.println(t);
+	}
+	public static void inBox(Box<Toy> box, Toy n){
+		box.set(n);	//상자에 넣기
+	}
+}
+
+class BoundedWildcardBase{
+	public static void main(String[] args){
+		Box<Toy> box = new Box<>();
+		BoxHandler.inBox(box, new Toy());
+		BoxHandler.outBox(box);
+	}
+}
