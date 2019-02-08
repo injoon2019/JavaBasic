fe5413466ae345a6d1a5a4f6402732719609e0a9
injoon2019 <injoon2019@gmail.com>
2019-02-08 00:41:04 -0800

    Feb 8th, 2019

---
+++ TwentythreeSlashTwo.java
@@ -0,0 +1,19 @@
+import java.util.TreeSet;
+import java.util.Iterator;
+import java.util.Comparator;
+
+class IntegerComparator implements Comparator<Integer>{
+	public int compare(Integer n1, Integer n2){
+		return n2 - n1;
+	}
+}
+class TwentythreeSlashTwo{
+	public static void main(String[] args){
+		TreeSet<Integer> tr = new TreeSet<>(new IntegerComparator());
+		tr.add(30);
+		tr.add(10);
+		tr.add(20);
+		System.out.println(tr);
+
+	}
+}
