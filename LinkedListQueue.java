e011a32ba788e2f0b1cc6b36c9db07ea15733453
injoon2019 <injoon2019@gmail.com>
2019-02-09 23:18:41 -0800

    Feb 9th, 2019

---
+++ LinkedListQueue.java
@@ -0,0 +1,25 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
+class LinkedListQueue{
+	public static void main(String[] args){
+		Queue<String> que = new LinkedList<>();	//LinkedList<E> 인스턴스 생성!
+		que.offer("Box");
+		que.offer("Toy");
+		que.offer("Robot");
+
+		//무엇이 다음에 나올지 확인
+		System.out.println("next: " +que.peek());
+
+		//첫 번째, 두번째 인스턴스 꺼내기
+		System.out.println(que.poll());
+		
+		System.out.println(que.poll());
+
+		//무엇이 다음에 나올지 확인
+		System.out.println("next: "+que.peek());
+
+		//마지막 인스턴스 꺼내기
+		System.out.println(que.poll());
+	}
+}
